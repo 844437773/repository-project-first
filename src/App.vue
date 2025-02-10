b378d176c02cb7be3d86deb9ab97668b67d2b1d2
高紫杰 <844437773@qq.com>
2025-02-10 11:14:40 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: '欢迎使用 Vue 3!'
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style> 
